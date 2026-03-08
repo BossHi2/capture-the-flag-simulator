348299f4593d36c2d1b18744170e6523a656d569
BossHi2 <157933871+BossHi2@users.noreply.github.com>
2026-03-08 17:23:55 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const inputNeurons = ["xPos", "yPos", "SNE", "xDNE","yDNE", "xDNT", "yDNT", "Str", "flag", "xPEF", "yPEF", "xPOF", "yPOF"]//, "SNE", "xDNE","yDNE", "xDNT", "yDNT", "Str", "flag", "xPEF", "yPEF", "xPOF", "yPOF"
+const inputNeurons = ["xPos", "yPos", "SNE", "xDNE","yDNE", "xDNT", "yDNT", "Str"]
 const internalNeurons = ["N1", "N2"]
 const outputNeurons = ["moveX", "moveY"]
 
@@ -7,12 +7,7 @@ const height = document.getElementsByClassName("game-board")[0].getBoundingClien
 
 const players = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
 
-const redFlagTop = document.getElementById("redFlag").getBoundingClientRect().top
-const redFlagBottom = document.getElementById("redFlag").getBoundingClientRect().bottom
-const redFlagRight = document.getElementById("redFlag").getBoundingClientRect().right
-const blueFlagTop = document.getElementById("blueFlag").getBoundingClientRect().top
-const blueFlagBottom = document.getElementById("blueFlag").getBoundingClientRect().bottom
-const blueFlagRight = document.getElementById("blueFlag").getBoundingClientRect().right
+
 
 const possibleGenomes = "abcdef0123456789"
 
@@ -20,6 +15,16 @@ const numOfGenesForOrganism = 30
 
 var blueScore = 0
 var redScore = 0
+
+var currGameLength = 0
+var totalGameLength = 1500 //15 seconds
+var mutationRate = 1 //random number btwn 1 and 10, if the number is less than or equal to mutationRate, it mutates
+
+document.getElementById("red-flag").style.backgroundColor = "rgba(255, 0, 0, 0.5)"
+document.getElementById("red-flag").style.left = ((window.innerWidth - width)/2) - 20 + "px"
+
+document.getElementById("blue-flag").style.backgroundColor = "rgba(0, 0, 255, 0.5)"
+document.getElementById("blue-flag").style.left = ((window.innerWidth - width)/2 + width) + "px"
 
 const spawnPoints = {
     red: {
@@ -61,6 +66,8 @@ class Player{
     geneHex
     geneBinary
 
+    flagsCaptured = 0//number of flags this object has captured
+
 
     xPosition //from top left
     yPosition //from top left
@@ -72,6 +79,9 @@ class Player{
     allNeurons = [];
 
     isBlue // true if on the blue team, false if on the red team
+
+    isInvincible = false
+    ticksSinceRespawned = 0
 
 
     collidingTickCounter = 0 //100 ticks = 1 second
@@ -120,13 +130,13 @@ class Player{
                     tempNeurons[0] = this.allNeurons[output[1]]
                 }
             }else{
-                var output2 = this.findNeuron(internalNeurons[parseInt(this.geneBinary.substring(i+1, i+8), 2)%internalNeurons.length])
-                if(output2[0] == false){
-                    var x2 = new InternalNeuron(internalNeurons[parseInt(this.geneBinary.substring(i+1, i+8), 2)%internalNeurons.length])
-                    tempNeurons[0] = x2
-                    this.allNeurons.push(x2)
+               var output = this.findNeuron(inputNeurons[parseInt(this.geneBinary.substring(i+1, i+8), 2)%inputNeurons.length])
+                if(output[0] == false){
+                    var x = new InputNeuron(inputNeurons[parseInt(this.geneBinary.substring(i+1, i+8), 2)%inputNeurons.length], this, weightVal)
+                    tempNeurons[0] = x
+                    this.allNeurons.push(x)
                 } else{
-                    tempNeurons[0] = this.allNeurons[output2[1]]
+                    tempNeurons[0] = this.allNeurons[output[1]]
                 }
 
             }
@@ -171,18 +181,13 @@ class Player{
     }
 
     doAction(){
-        if(this.hasFlag){
-            if(this.isBlue){
-                document.getElementById(this.id).style.left = (Number.parseInt(window.getComputedStyle(document.getElementById(this.id)).left.substring(0, window.getComputedStyle(document.getElementById(this.id)).left.indexOf("px"))) + 5) + "px"
-            } else{
-                document.getElementById(this.id).style.left = (Number.parseInt(window.getComputedStyle(document.getElementById(this.id)).left.substring(0, window.getComputedStyle(document.getElementById(this.id)).left.indexOf("px"))) - 5) + "px"
-            }
-            
-            this.updateHasFlag()
-            
-            
-        } else{
-            for(const[key, value] of this.neurons){
+        this.ticksSinceRespawned++
+        if(this.ticksSinceRespawned <= 50)
+            this.isInvincible = true
+        else
+            this.isInvincible = false
+        this.updateHasFlag()
+        for(const[key, value] of this.neurons){
                 if(key instanceof InternalNeuron){
                     key.sum = 0
                 } if(value instanceof InternalNeuron || value instanceof OutputNeuron){
@@ -214,12 +219,19 @@ class Player{
                     value.sum += key.sum
                 }
             }
-
+        if(this.hasFlag){
+            if(this.findOverlap()){
+                if(this.collidingEnemyIndex != -1)
+                    this.fighting()
+            } else{
+                if(this.isBlue){
+                document.getElementById(this.id).style.left = (Number.parseInt(window.getComputedStyle(document.getElementById(this.id)).left.substring(0, window.getComputedStyle(document.getElementById(this.id)).left.indexOf("px"))) + 3) + "px"
+                } else{
+                    document.getElementById(this.id).style.left = (Number.parseInt(window.getComputedStyle(document.getElementById(this.id)).left.substring(0, window.getComputedStyle(document.getElementById(this.id)).left.indexOf("px"))) - 3) + "px"
+                }
+            }
             
-
-
-
-
+        } else{
             var movementEncourage = [0,0] //[x,y]
             for(const[key, value] of this.neurons){
                 if(value instanceof OutputNeuron){
@@ -285,12 +297,15 @@ class Player{
             }
 
             
-            this.updateHasFlag()
+            
         }
 
 
-        if((this.collidingTickCounter/100) == 3){   //if it has collided for 3 seconds
+        if(document.getElementById(this.id).children[0].style.width == "0px"){   //if it has collided for 3 seconds
             this.respawn()
+            this.hasFlag = false
+            document.getElementById(this.id).classList.remove("hasRedFlag")
+            document.getElementById(this.id).classList.remove("hasBlueFlag")
         }
 
         if(this.collidingTickCounter == 0){
@@ -303,7 +318,12 @@ class Player{
     }
 
     respawn(){
+        this.ticksSinceRespawned = 0
         this.collidingTickCounter = 0
+        if(this.collidingEnemyIndex != -1){
+            players[this.collidingEnemyIndex].strength += 0.1
+            this.strength -= 0.1
+        }
         var spawnNum = this.isBlue ? Number.parseInt(this.id.substring(5)) : Number.parseInt(this.id.substring(4))
 
         if(this.isBlue){
@@ -323,7 +343,7 @@ class Player{
     fighting(){
         this.collidingTickCounter++
         if(this.collidingEnemyIndex != -1){
-            if(players[this.collidingEnemyIndex].strength > this.strength){
+            if(players[this.collidingEnemyIndex].strength >= this.strength){
                 document.getElementById(this.id).children[0].style.width = 20-(this.collidingTickCounter/15) + "px"
             }
         } else
@@ -348,7 +368,7 @@ class Player{
             if(players[i].id == this.id)
                 continue
 
-            if(this.detectOverlap(otherPlayer, currPlayer) && (players[i].isBlue != this.isBlue)){
+            if(this.detectOverlap(otherPlayer, currPlayer) && (players[i].isBlue != this.isBlue) && this.isInvincible == false && players[i].isInvincible == false){
                 this.collidingEnemyIndex = i
                 return true
             }
@@ -359,40 +379,24 @@ class Player{
 
     updateHasFlag(){
         if(this.hasFlag == false){
-            var enemyFlagId = this.isBlue ? "redFlag" : "blueFlag"
-            var playerRect = document.getElementById(this.id).getBoundingClientRect()
-            var flagRect = document.getElementById(enemyFlagId).getBoundingClientRect()
-
-            if(this.detectOverlap(playerRect, flagRect)) {
+            if(this.isBlue && this.xPosition <= 5){
                 this.hasFlag = true
-                if(this.isBlue)
-                    document.getElementById(this.id).classList.add("hasRedFlag")
-                else
-                    document.getElementById(this.id).classList.add("hasBlueFlag")
+                document.getElementById(this.id).classList.add("hasRedFlag")
+            } else if(this.isBlue == false && this.xPosition >= 825){
+                this.hasFlag = true
+                document.getElementById(this.id).classList.add("hasBlueFlag")
             }
         } else{
-            var ownFlagId = this.isBlue ? "blueFlagRadius" : "redFlagRadius"
-            var playerRect = document.getElementById(this.id).getBoundingClientRect()
-            var flagRect = document.getElementById(ownFlagId).getBoundingClientRect()
-
-            if(this.detectOverlap(playerRect, flagRect)) {
+            if(this.isBlue && this.xPosition >= 825){
                 this.hasFlag = false
-                if(this.isBlue)
-                    blueScore++
-                else
-                    redScore++
-                document.getElementsByClassName("score")[0].innerHTML = "red: " + redScore + " | blue: " + blueScore
-
-                if(blueScore == 5){
-                    repopulate("blue")
-                } else if(redScore == 5){
-                    repopulate("red")
-                }
-                if(this.isBlue)
-                    document.getElementById(this.id).classList.remove("hasRedFlag")
-                else
-                    document.getElementById(this.id).classList.remove("hasBlueFlag")
+                document.getElementById(this.id).classList.remove("hasRedFlag")
+                blueScore++
+            } else if(this.isBlue == false && this.xPosition <= 5){
+                this.hasFlag = false
+                redScore++
+                document.getElementById(this.id).classList.remove("hasBlueFlag")
             }
+            document.getElementsByClassName("score")[0].innerHTML = "red: " + redScore + " | blue: " + blueScore
         }
         
     }
@@ -427,35 +431,6 @@ class InputNeuron{
             this.data = this.findNearestPlayer(false)[2]/height
         } else if(this.name == "Str"){
             this.data = this.player.strength
-        } else if(this.name == "flag"){
-            if(this.player.hasFlag)
-                this.data = 1
-            else
-                this.data = 0
-        } else if(this.name == "xPEF"){
-            if(this.player.isBlue){
-                this.data = redFlagRight/width
-            } else{
-                this.data = blueFlagRight/width
-            }
-        } else if(this.name == "yPEF"){
-            if(this.player.isBlue){
-                this.data = redFlagBottom/height
-            } else{
-                this.data = blueFlagBottom/height
-            }
-        } else if(this.name == "xPOF"){
-            if(this.player.isBlue){
-                this.data = blueFlagRight/width
-            } else{
-                this.data = redFlagRight/width
-            }
-        } else if(this.name == "yPOF"){
-            if(this.player.isBlue){
-                this.data = blueFlagBottom/height
-            } else{
-                this.data = redFlagBottom/height
-            }
         }
     }
 
@@ -524,9 +499,133 @@ class OutputNeuron{
 }
 
 function runGame(){
+    if(currGameLength == 0 && document.getElementsByClassName("game-timer").length == 0){
+        var div = document.createElement("div")
+        div.className = "game-timer"
+        document.body.appendChild(div)
+        div.innerHTML = "Time left: 15 seconds"
+    }
+    currGameLength++
     for(let i = 0; i<20; i++){
         players[i].doAction()
     }
+    document.getElementsByClassName("game-timer")[0].innerHTML = "Time left: " + (15- Math.floor(currGameLength/100)) + " seconds"
+    if(currGameLength >= totalGameLength){
+        document.getElementsByClassName("game-timer")[0].innerHTML = "Time left: 0 seconds"
+        currGameLength = 0
+        naturalSelection()
+    }
+}
+
+function naturalSelection(){
+    clearInterval(interval)
+
+
+    var blueFitness = [0, -1, 0, -1]  //[0] = fitness, [1] = index [2] = fitness, [3] = index
+    var redFitness = [0, -1, 0, -1]  //[0] = fitness, [1] = index
+    for(let i = 0; i<players.length; i++){
+        var currFitness = players[i].flagsCaptured * 10
+        if(i < 10 && currFitness > blueFitness[0]){
+            blueFitness[0] = currFitness
+            blueFitness[1] = i
+        }else if(i >= 10 && currFitness > redFitness[0]){
+            redFitness[0] = currFitness
+            redFitness[1] = i
+        } else if(i < 10 && currFitness > blueFitness[2]){
+            blueFitness[2] = currFitness
+            blueFitness[3] = i
+        }else if(i >= 10 && currFitness > redFitness[2]){
+            redFitness[2] = currFitness
+            redFitness[3] = i
+        }
+    }
+
+
+    console.log(redFitness)
+    for(let i = 0; i<players.length; i++){
+        if(i != blueFitness[1] && i != redFitness[1] && i != blueFitness[3] && i != redFitness[3]){
+            document.getElementById(players[i].id).remove()
+            players[i] = null
+        }
+    }
+
+    var team = ""
+    var isBlue;
+    var numOfReproductions = 0
+    for(let i = 0; i< 20; i++){
+        if(players[i] == null){
+            var index = i%10 + ""
+            var player = document.createElement("div")
+            player.classList.add("player")
+            if(i<10){
+                team = "red"
+                isBlue = false
+                player.style.left = spawnPoints.red[index][0] + "px"
+                player.style.top = spawnPoints.red[index][1] + "px"
+            } else{
+                team = "blue"
+                isBlue = true
+                player.style.left = spawnPoints.blue[index][0] + "px"
+                player.style.top = spawnPoints.blue[index][1] + "px"
+            }
+            player.id = team + "-" + (i%10)
+            var countdown = document.createElement("div")
+            countdown.classList.add("countdown")
+            player.appendChild(countdown)
+            document.getElementsByClassName("game-board")[0].appendChild(player)
+            var randomNum = Math.floor(Math.random()*10) + 1
+            
+            var currGeneHex = null
+            if((blueFitness[1] == -1 || blueFitness[3] == -1) && isBlue){
+                var randomGenome = ""
+                for(let r = 0; r<numOfGenesForOrganism*8; r++){
+                    randomGenome += possibleGenomes.charAt(Math.floor(Math.random() * possibleGenomes.length))
+                }
+                players[i] = new Player(Math.random(), team + "-" + (i%10), randomGenome, isBlue)
+            } else if((redFitness[1] == -1 || redFitness[3] == -1) && isBlue == false){
+                var randomGenome = ""
+                for(let r = 0; r<numOfGenesForOrganism*8; r++){
+                    randomGenome += possibleGenomes.charAt(Math.floor(Math.random() * possibleGenomes.length))
+                }
+                players[i] = new Player(Math.random(), team + "-" + (i%10), randomGenome, isBlue)
+            } else{
+                numOfReproductions++
+                if(i == 10)
+                    numOfReproductions = 0
+                if(isBlue && numOfReproductions < 5)
+                    currGeneHex = players[blueFitness[1]].geneHex
+                else if(isBlue && numOfReproductions >= 5){
+                    currGeneHex = players[blueFitness[3]].geneHex
+                } else if(isBlue == false && numOfReproductions < 5)
+                    currGeneHex = players[redFitness[1]].geneHex
+                else if(isBlue == false && numOfReproductions >= 5){
+                    currGeneHex = players[redFitness[3]].geneHex
+                }
+                if(randomNum <= mutationRate){
+                    var randIndex = Math.floor(Math.random() * (currGeneHex.length-2)) + 1
+                    currGeneHex = currGeneHex.substring(0, randIndex) + possibleGenomes[Math.floor(Math.random()*possibleGenomes.length)] + currGeneHex.substring(randIndex+1)
+                }
+
+                players[i] = new Player(Math.random(), team + "-" + (i%10), currGeneHex, isBlue)
+            }
+            
+        }
+        
+    }
+
+     for(let i = 0; i< 20; i++){
+        var player = document.getElementById(players[i].id)
+        var index = i%10 + ""
+        if(i<10){
+            player.style.left = spawnPoints.red[index][0] + "px"
+            player.style.top = spawnPoints.red[index][1] + "px"
+        } else{
+            player.style.left = spawnPoints.blue[index][0] + "px"
+            player.style.top = spawnPoints.blue[index][1] + "px"
+        }
+    }
+
+    interval = setInterval(runGame, 10)
 }
 
 function repopulate(winningTeam){
@@ -601,6 +700,7 @@ function start(){
 
     interval = setInterval(runGame, 10)
 }
+
 
 function bob(){
     /*for(var i = 0; i<20; i++){
